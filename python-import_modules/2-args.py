d21b1292acb6d4d34037b09a4e3058492faf2b69
Sean <13078@holbertonstudents.com>
2026-05-06 06:46:53 +0000

    push args task

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,11 +2,9 @@
 if __name__ == "__main__":
     import sys
 
-    # Get arguments excluding the script name
     argv = sys.argv[1:]
     count = len(argv)
 
-    # Logic for header punctuation and pluralization
     if count == 0:
         print("0 arguments.")
     elif count == 1:
@@ -14,6 +12,5 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(count))
 
-    # Print each argument with its 1-based index
     for i in range(count):
         print("{}: {}".format(i + 1, argv[i]))
