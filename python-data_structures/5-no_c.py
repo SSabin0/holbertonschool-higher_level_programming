63cea97850335a957f364d4f317854b27d3fe2dd
Sean <13078@holbertonstudents.com>
2026-05-16 16:52:39 +0000

    fixed no c

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    # 1. Start with an empty string container
-    final_string = ""
-    
+    new_string = ""
     for i in range(len(my_string)):
         if my_string[i] != 'c' and my_string[i] != 'C':
-            # 2. Save the character into your container instead of printing it
-            final_string += my_string[i]
-            
-    # 3. Return the actual built string, NOT an empty one!
-    return final_string
+            new_string += my_string[i]
+    return new_string
