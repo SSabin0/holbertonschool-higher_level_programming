116ec7efe2485971a6283ccedb244e614f5f797e
Sean <13078@holbertonstudents.com>
2026-05-19 12:42:49 +0000

    fix search and replace

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    """Replaces all occurrences using a list comprehension."""
-    return [replace if x == search else x for x in my_list]
+    """Replaces all occurrences of an element by another in a new list."""
+    new_list = []
+    for i in range(len(my_list)):
+        if my_list[i] == search:
+            new_list.append(replace)
+        else:
+            new_list.append(my_list[i])
+    return new_list
