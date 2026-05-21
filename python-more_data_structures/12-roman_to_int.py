75b4625428ccfeec2600ce5b8b4b44ecc0816136
SSabin0 <13078@holbertonstudents.com>
2026-05-21 17:31:12 +1000

    Update 12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,25 +1,19 @@
 #!/usr/bin/python3
-"""Module for converting Roman numerals to integers."""
-
-
 def roman_to_int(roman_string):
-    """Convert a roman string to an integer."""
-    roman_dict = {
-        "I": 1,
-        "V": 5,
-        "X": 10,
-        "L": 50,
-        "C": 100,
-        "D": 500,
-        "M": 1000
-    }
+    """Converts a Roman numeral string to an integer."""
+    if not isinstance(roman_string, str) or not roman_string:
+        return 0
 
+    roman_dict = {
+        "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000
+    }
     total = 0
     length = len(roman_string)
 
     for i in range(length):
         current_val = roman_dict.get(roman_string[i], 0)
 
+        # Look ahead to the next numeral if it exists
         if i + 1 < length and current_val < roman_dict.get(roman_string[i + 1], 0):
             total -= current_val
         else:
