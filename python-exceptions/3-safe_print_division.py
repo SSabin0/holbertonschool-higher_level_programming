ccdb7476fa7d435c390387477b0658c0a1eeb111
SSabin0 <13078@holbertonstudents.com>
2026-05-21 18:48:46 +1000

    Create 3-safe_print_division.py

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    """Divides 2 integers and prints the result inside finally."""
+    result = None
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        pass
+    finally:
+        print("Inside result: {}".format(result))
+
+    return result
