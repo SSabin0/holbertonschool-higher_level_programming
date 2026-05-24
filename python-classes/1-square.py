4b40a171015cef04341a8b31245aba148ac70173
SSabin0 <13078@holbertonstudents.com>
2026-05-25 01:40:34 +1000

    Update 1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,3 +1,12 @@
+#!/usr/bin/python3
+"""Module that defines a Square class."""
 class Square:
+    """A class that defines a square by its size."""
+
     def __init__(self, size):
-        self.__size = size  # This is a private instance attribute
+        """Initialize a new Square instance.
+
+        Args:
+            size: The size of the side of the square.
+        """
+        self.__size = size
