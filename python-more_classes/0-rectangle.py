52ce6829aa7bed3b3c6ff5376674c1242603c75e
Kajimone <86360848+Kajimone@users.noreply.github.com>
2026-05-29 18:03:27 +1000

    Fix: Force execution permission bit

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""This is a module defines an empty class Rectangle."""
+"""This module defines an empty class Rectangle."""
+
 
 class Rectangle:
     """An empty class that defines a rectangle."""
