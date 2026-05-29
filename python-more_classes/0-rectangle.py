f84e3240a970d50c9e66c57ea4d595208d040b47
SSabin0 <13078@holbertonstudents.com>
2026-05-29 17:42:37 +1000

    correct indention and remove excess newline

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""
-This module defines an empty class Rectangle.
-"""
+"""This module defines an empty class Rectangle."""
+
 
 class Rectangle:
     """An empty class that defines a rectangle."""
+
     pass
