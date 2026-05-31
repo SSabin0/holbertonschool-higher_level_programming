a2f32652ff885ed707919214c63956c26a55a822
Sean <13078@holbertonstudents.com>
2026-05-31 08:36:52 +0000

    4-rectangle.py

---
+++ python-more_classes/4-rectangle.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""Module that defines a Rectangle class with string and eval support."""
+
+
+class Rectangle:
+    """A class that represents a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle instance."""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Retrieve the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width of the rectangle with validation."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieve the height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height of the rectangle with validation."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Calculate and return the rectangle area."""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Calculate and return the rectangle perimeter."""
+        if self.width == 0 or self.height == 0:
+            return 0
+        return (self.width + self.height) * 2
+
+    def __str__(self):
+        """Return a string representation of the rectangle using #."""
+        if self.width == 0 or self.height == 0:
+            return ""
+        rect_rows = ["#" * self.width for _ in range(self.height)]
+        return "\n".join(rect_rows)
+
+    def __repr__(self):
+        """Return a string representation to recreate the instance."""
+        return "Rectangle({}, {})".format(self.width, self.height)
