2fdcab6fa3afc006616f0811329a85f712b76b56
SSabin0 <13078@holbertonstudents.com>
2026-06-18 17:06:07 +1000

    Update 0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,7 +2,6 @@
 """
 This module contains a function that reads a text file and prints its content.
 """
-
 def read_file(filename=""):
     """Reads a UTF8 text file and prints its content to stdout."""
     with open(filename, "r", encoding="utf-8") as f:
