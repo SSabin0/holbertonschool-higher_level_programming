d55035fc220fc695814b79daf7903e865ee6ae52
SSabin0 <13078@holbertonstudents.com>
2026-06-18 17:32:52 +1000

    Create 5-save_to_json_file.py

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module defines a function that saves an object to a file as JSON."""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Writes an object to a text file using a JSON representation.
+
+    Args:
+        my_obj: The Python object to serialize and save.
+        filename (str): The name of the file to write to.
+    """
+    with open(filename, mode="w", encoding="utf-8") as f:
+        json.dump(my_obj, f)
