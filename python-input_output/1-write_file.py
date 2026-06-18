bf8c4a3b561cd00d242d4c276cc933cface47567
SSabin0 <13078@holbertonstudents.com>
2026-06-18 17:18:06 +1000

    Update 1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 """
-This module contains a function that appends a string to a text file (UTF8)
-and returns the number of characters added.
+This module contains a function that writes a string to a text file (UTF8)
+and returns the number of characters written.
 """
 
 
-def append_write(filename="", text=""):
-    """Appends a string to the end of a UTF8 text file and returns characters added.
+def write_file(filename="", text=""):
+    """Writes a string to a UTF8 text file and returns the character count.
 
     Args:
-        filename (str): The name of the file to append to.
-        text (str): The text string to append.
+        filename (str): The name of the file to write to.
+        text (str): The text to write into the file.
 
     Returns:
-        int: The number of characters added.
+        int: The number of characters written to the file.
     """
-    with open(filename, mode="a", encoding="utf-8") as f:
+    with open(filename, mode="w", encoding="utf-8") as f:
         return f.write(text)
