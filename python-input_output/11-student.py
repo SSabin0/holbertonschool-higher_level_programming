4ae9d1d6ff1dcfb4a378d35fc22a03e67122bcd8
SSabin0 <13078@holbertonstudents.com>
2026-06-18 17:51:19 +1000

    Create 11-student.py

---
+++ python-input_output/11-student.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""This module defines a Student class with serialization and deserialization
+
+capabilities.
+"""
+
+
+class Student:
+    """Represents a student by name and age."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new Student instance.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance.
+
+        If attrs is a list of strings, only attributes contained in this
+        list are retrieved. Otherwise, all attributes are retrieved.
+
+        Args:
+            attrs (list, optional): A list of strings representing the
+                attributes to retrieve. Defaults to None.
+
+        Returns:
+            dict: A dictionary containing the filtered or unfiltered
+                attributes of the student.
+        """
+        if isinstance(attrs, list) and all(isinstance(x, str) for x in attrs):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance.
+
+        Args:
+            json (dict): A dictionary where keys match public attribute
+                names and values represent their new values.
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
