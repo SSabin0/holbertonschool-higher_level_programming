f4cf3902692699709fc74df26059b6473a53b464
SSabin0 <13078@holbertonstudents.com>
2026-06-22 19:30:24 +1000

    Create task_00_basic_serialization.py

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,11 @@
+import json
+
+def serialize_and_save_to_file(data, filename):
+    """Serializes a Python dictionary and saves it to a JSON file."""
+    with open(filename, mode="w", encoding="utf-8") as f:
+        json.dump(data, f)
+
+def load_and_deserialize(filename):
+    """Loads a JSON file and deserializes it back into a Python dictionary."""
+    with open(filename, mode="r", encoding="utf-8") as f:
+        return json.load(f)
