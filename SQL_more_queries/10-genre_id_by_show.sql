a928a0ef51eab7e9b243ca85431b78d2968b670c
SSabin0 <13078@holbertonstudents.com>
2026-07-11 00:12:07 +1000

    Enhance SQL query with explanatory comments
    Added comments to clarify the SQL query's purpose and functionality.

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,7 @@
+-- Select the title of the show and the corresponding genre ID
 SELECT tv_shows.title, tv_show_genres.genre_id
+  -- Use an INNER JOIN to connect the shows to their linked genres
+-- This naturally filters out any shows that have no genres assigned
 FROM tv_shows
 INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
