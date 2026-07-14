704c9ba3f514ecc7790f15f1531a210c369e66a4
SSabin0 <13078@holbertonstudents.com>
2026-07-14 16:49:10 +1000

    Refactor database connection and cursor usage

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,19 +1,29 @@
 #!/usr/bin/python3
+"""Script that lists all states from a MySQL database, sorted by id."""
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
-    db = MySQLdb.connect(
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    conn = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
+        user=username,
+        passwd=password,
+        db=db_name,
+        charset="utf8"
     )
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cursor.fetchall()
+
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+
     for row in rows:
         print(row)
-    cursor.close()
-    db.close()
+
+    cur.close()
+    conn.close()
