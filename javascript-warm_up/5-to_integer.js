8fd42563a153967ea15ec0e415cb40c2ea7907ec
SSabin0 <13078@holbertonstudents.com>
2026-07-23 15:19:14 +1000

    add to integer

---
+++ javascript-warm_up/5-to_integer.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+
+if (isNaN(argv[2]) === true) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + parseInt(argv[2]));
+}
